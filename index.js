6e6bbf7e2abd7a584dfca003f359fda72131b606
Nitin Tembhare <nitintembhare340@gmail.com>
2024-06-12 15:24:30 +0530

    MongoDb connected

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
+const express = require("express");
+const mongoose = require("mongoose");
+require("dotenv").config();
 
+// imports
+const db = require("./db");
+
+// constants
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+//middlewares
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.json("message : Welcome to Ebook apis");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running at port ${PORT}`);
+});
